ab7378e69e946fbc270cc64e203c37d7333d81f2
zhangddy <530281461@qq.com>
2019-11-02 11:36:55 +0800

    test

---
+++ 191028_test/191028_test/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+#include <string.h>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+	cout << sizeof(int*) << endl;
+	system("pause");
+	return 0;
+}
+
+
+
