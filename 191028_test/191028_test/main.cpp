09d51470c1efd5c3d0cc71f7f3238edbeb28905a
zhangddy <530281461@qq.com>
2019-11-07 22:26:46 +0800

    test

--- 191028_test/191028_test/main.cpp
+++ 191028_test/191028_test/main.cpp
@@ -1,15 +1,55 @@
 #include <iostream>
+#include <string>
 #include <cstdlib>
-#include <string.h>
-#include <cstdio>
+#include <vector>
 using namespace std;
+
+class Solution {
+public:
+	int MoreThanHalfNum_Solution(vector<int> numbers) {
+
+		int i = numbers[0], num = 1;
+		for (int j = 1; j < numbers.size(); ++j)
+		{
+			if (i != numbers[j])
+			{
+				if (num == 1)
+				{
+					i = numbers[j];
+				}
+				else
+				{
+					num--;
+				}
+			}
+			else
+			{
+				num++;
+			}
+		}
+		int x = 0;
+		for (int j = 0; j < numbers.size(); ++j)
+		{
+			if (numbers[j] == i)
+			{
+				x++;
+			}
+		}
+		if (x * 2 > numbers.size())
+			return i;
+		else
+		{
+			return 0;
+		}
+	}
+};
 
 int main()
 {
-	cout << sizeof(int*) << endl;
+	Solution s;
+	vector<int> v = { 1,2,3,5,4,2,2,2,3};
+	cout << s.MoreThanHalfNum_Solution(v);
+
 	system("pause");
 	return 0;
 }
-
-
-
--- 191028_test/191028_test/test.cpp
+++ 191028_test/191028_test/test.cpp
@@ -1,1 +1,39 @@
-#include "test.h"
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+
+
+int main2()
+{
+	string s1("");
+	string s2("");
+
+	getline(cin, s1);
+
+	int front;
+	int back;
+
+	
+	while (s1.find(' ') != s1.npos)
+	{
+		back = s1.size();
+		front = s1.rfind(' ');
+		s2.append(s1, front, back);
+		s1.resize(front);
+	}
+	s2 += ' ';
+	s2.append(s1);
+
+	int size = s2.size();
+	for (size_t i = 0; i < s2.size() - 1; ++i)
+	{
+		s2[i] = s2[i + 1];
+	}
+	s2.resize(size - 1);
+	cout << s2 << endl;
+
+	system("pause");
+	return 0;
+}
