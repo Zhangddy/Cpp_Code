347cf1f0d734c61eaf018904a3c96bb3657458fc
zhangddy <530281461@qq.com>
2019-11-16 23:16:33 +0800

    test

--- 191028_test/191028_test/main.cpp
+++ 191028_test/191028_test/main.cpp
@@ -3,10 +3,34 @@
 #include <cstdlib>
 
 using namespace std;
+int func(int m, int n)
+{
+	if (n == 0 && m == 0)
+	{
+		return 0;
+	}
+	if (n == 1 && m == 0)
+	{
+		return 1;
+	}
+	if (m == 1 && n == 0)
+	{
+		return 1;
+	}
+	if (n == 0)
+	{
+		return func(m - 1, n);
+	}
+	if (m == 0)
+	{
+		return func(m, n - 1);
+	}
+	return func(m - 1, n) + func(n, m - 1);
+}
 
 int main()
 {
-	
+	cout << func(2, 2);
 	system("pause");
 	return 0;
 }
--- 191028_test/191028_test/test.cpp
+++ 191028_test/191028_test/test.cpp
@@ -1,42 +1,30 @@
 #include <iostream>
 #include <cstdlib>
-#include <string>
-#include <stack>
+
 using namespace std;
+class UnusualAdd {
+public:
+	int addAB(int A, int B) {
+		// write code here
 
+		int sum, carry;
 
+		do
+		{
+			sum = A ^ B;
+			carry = (A & B) << 1;
 
+			A = sum;
+			B = carry;
+		} while (B != 0);
+		
+		return sum;
+	}
+};
 int main1()
 {
-	string String;
-	int N;
-	stack<char> Stack;
-	cin >> String >> N;
-	bool status = true;
-
-	for (size_t i = 0; i < String.size(); i++)
-	{
-		if (String[i] == '(')
-		{
-			Stack.push(String[i]);
-		}
-		else if (String[i] == ')')
-		{
-			if (Stack.size() == 0 || Stack.top() != '(')
-			{
-				status = false;
-			}
-			else
-			{
-				Stack.pop();
-			}
-		}
-	}
-	if (Stack.size() != 0)
-	{
-		status = false;
-	}
-	cout << status << endl;
+	UnusualAdd ua;
+	cout << ua.addAB(1, 2);
 	system("pause");
 	return 0;
 }
