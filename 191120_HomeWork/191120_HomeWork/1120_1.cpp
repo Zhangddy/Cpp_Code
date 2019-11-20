490c275275630c2cf02aa26f24ea8fed4ef03e07
zhangddy <530281461@qq.com>
2019-11-20 23:10:39 +0800

    OJ

---
+++ 191120_HomeWork/191120_HomeWork/1120_1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+
+
+int main()
+{
+
+	int year, month, day;
+	while (cin >> year >> month >> day)
+	{
+		int ret = 0;
+		// 判断是不是润年
+		int date[] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+		bool status = false;
+		if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+		{
+			status = true;
+			date[1]++;
+		}
+
+		// 将day加入结果
+		ret += day;
+		// 将month加入结果
+		for (size_t i = 0; i < month - 1; i++)
+		{
+			ret += date[i];
+		}
+		cout << ret << endl;
+	}
+	system("pause");
+	return 0;
+}
